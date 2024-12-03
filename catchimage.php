43c23da10a80bc7603c7eb50b0ab5e2ddb125d9c
PetersOtto <52236754+PetersOtto@users.noreply.github.com>
2024-12-03 11:13:07 +0100

    Update catchimage.php
    Cleaned up the source code.

--- catchimage.php
+++ catchimage.php
@@ -1,7 +1,7 @@
 <?php
 class YellowCatchImage
 {
-    const VERSION = '0.9.1';
+    const VERSION = '0.9.4';
 
     public $yellow;  // access to API
 
@@ -9,120 +9,201 @@ class YellowCatchImage
     public function onLoad($yellow)
     {
         $this->yellow = $yellow;
-        $this->yellow->system->setDefault('catchImageDefaultAltText', 'Lesen Sie den Beitrag:');
+        $this->yellow->system->setDefault('catchImageDefaultAltText', 'Lesen Sie den Beitrag: ');
     }
 
+    // Start with main method
     public function getCatchImage($filenametype, $baseUrl, $catchImageAltText, $catchImageFilter, $catchImageTitle)
     {
         $output = null;
         $srcNew = null;
-        $filterAvailableInternal = null;
-        $filterAvailableExternal = null;
-        $choosedFilter = null;
-        $isFilterAvailableExternal = null;
-        $isFilterAvailableInternal = null;
         $srcNewInside = null;
-        $supportedType = null;
-        $defaultFilter = strtolower($this->yellow->system->get('imageFilterDefaultImfi'));
-        $defaultFilter = explode('-', $defaultFilter);
-        $defaultFilter = preg_replace('/\s+/', '', $defaultFilter[1]);
+        $srcOriginal = $this->yellow->lookup->findMediaDirectory('coreImageLocation') . $filenametype;
+
+        // Check if »webp« should use
         $useWebp = $this->yellow->system->get('imageFilterUseWebp');
+
+        // Get image width and height.
+        list($width, $height, $type, $widthAndHeight) = getimagesize($srcOriginal);
+    
+        // Split filename into name and type
         $splitFilenameType = explode('.', $filenametype);
         $originalFilename = $splitFilenameType[0];
         $originalType = $splitFilenameType[1];
-        $srcOriginal = $this->yellow->lookup->findMediaDirectory('coreImageLocation') . $filenametype;
+        $isTypeAllowed = $this->yellow->extension->get('imagefilter')->checkIfTypeIsAllowed($originalType);
 
-        if ($originalType == 'jpeg' || $originalType == 'jpg' || $originalType == 'png' || $originalType == 'webp') {
-            $supportedType = '1';
-        } else {
-            $supportedType = '0';
-        }
-
-        if (empty($catchImageFilter)) {
-            $choosedFilter = $defaultFilter;
-        } else {
-            $catchImageFilter = strtolower($catchImageFilter);
-            if (strpos($catchImageFilter, 'imfi') !== false) {
-                $catchImageFilter = explode('-', $catchImageFilter);
-                $catchImageFilter = preg_replace('/\s+/', '', $catchImageFilter[1]);
-                $choosedFilter = $catchImageFilter;
-            } else {
-                $choosedFilter = $defaultFilter;
+        // Check if filter is given in posts header
+        // Get it without »imfi-« and eliminate wrong choosed »imfi-webp«
+        if (!empty($catchImageFilter)) {
+            $catchImageFilter = $this->getFilter($catchImageFilter);
+            if ($catchImageFilter === 'webp') {
+                $catchImageFilter = '';
             }
         }
 
-        if ($choosedFilter == 'original' && $this->yellow->system->get('imageFilterUseWebp') == 1) {
+        // Get »$defaultFilter« and set it to »$choosedFilter«
+        // Eliminate wrong choosed »imfi-webp« in »system.ini«
+        $defaultFilter = $this->getFilter($this->yellow->system->get('imageFilterDefaultImfi'));
+        if ($defaultFilter === 'webp') {
+            $defaultFilter = 'original';
+        }
+        $choosedFilter = $defaultFilter;
+
+        // Check if »$defaultFilter« and »$catchImageFilter« have insert »original«
+        $compareDefaultCatchImageOriginal = $this->checkIfTwoOriginal($defaultFilter, $catchImageFilter);
+
+        // Check if filter is available in extension »ImageFilter« or »ImageFilterCollection«
+        $isFilterCatchAvailableExternal = $this->checkIfFilterIsAvailableExternal($catchImageFilter); 
+        $isFilterCatchAvailableInternal = $this->checkIfFilterIsAvailableInternal($catchImageFilter);
+        $isFilterDefaultAvailableExternal = $this->checkIfFilterIsAvailableExternal($defaultFilter);
+        $isFilterDefaultAvailableInternal = $this->checkIfFilterIsAvailableInternal($defaultFilter);
+        $isFilterDefaultAvailable = $this->checkIfFilterDefaultIsAvailable($defaultFilter);
+        $isFilterAvailable = $this->checkIfFilterIsAvailable($isFilterCatchAvailableExternal, $isFilterCatchAvailableInternal, $isFilterDefaultAvailable); 
+        
+        if ($isFilterAvailable && !empty($catchImageFilter)) {                                                                
+            $choosedFilter = $catchImageFilter; 
+        }
+
+        // If »defaultFilter« is not available
+        if ($isFilterDefaultAvailable === false) {                                                                
+            $choosedFilter = 'original'; 
+            $isFilterAvailable = true;
+        }
+
+        // If filter is available or »$catchImageFilter == 'original'« set it as choosed.
+        // If not -> »choosedFilter« stay »$defaultFilter«
+        if ($isFilterAvailable && !empty($catchImageFilter)) {                                                                
+            $choosedFilter = $catchImageFilter; 
+        }
+        
+        // Check if »webp« should use
+        if ($choosedFilter == 'original' &&  $useWebp == true && $isTypeAllowed === true ) {
             $choosedFilter = 'webp';
+            $isFilterAvailable = true;
         }
-        if ($choosedFilter == 'original' && $this->yellow->system->get('imageFilterUseWebp') == 0) {
+
+        // Generate new Filename
+        $newFilename = '/' . $originalFilename . '-' . $choosedFilter;
+
+        // Use original image without »webp«
+        if ((($choosedFilter == 'original' || $choosedFilter == 'webp') &&  $useWebp == false) || $isTypeAllowed === false || $compareDefaultCatchImageOriginal === true  ) {
             $choosedFilter = '';
-        }
-
-        $newFilename = $originalFilename . '-' . $choosedFilter;
-
-        $isFilterAvailableExternal = $this->checkIfFilterIsAvailableExternal($choosedFilter);
-        $isFilterAvailableInternal = $this->checkIfFilterIsAvailableInternal($choosedFilter);
-
-        if ($this->yellow->system->get('imageFilterUseWebp') == 1 && ($isFilterAvailableExternal || $isFilterAvailableInternal)) {
-            $srcNewInside = $this->yellow->lookup->findMediaDirectory('coreImageLocation') . "$choosedFilter/" . $newFilename . '.webp';
-            $srcNew = $baseUrl . $this->yellow->system->getHTML('CoreImageLocation') . "$choosedFilter/" . $newFilename . '.webp';
-        }
-
-        if ($this->yellow->system->get('imageFilterUseWebp') != 1 && ($isFilterAvailableExternal || $isFilterAvailableInternal || empty($choosedFilter))) {
-            $srcNewInside = $this->yellow->lookup->findMediaDirectory('coreImageLocation') . "$choosedFilter/" . $newFilename . '.' . $originalType;
-            $srcNew = $baseUrl . $this->yellow->system->getHTML('CoreImageLocation') . "$choosedFilter/" . $newFilename . '.' . $originalType;
-        }
-
-        if ($supportedType == '0') {
             $newFilename = $originalFilename;
         }
 
-        if ($supportedType == '0') {
-            $srcNewInside = $this->yellow->lookup->findMediaDirectory('coreImageLocation') . $newFilename . '.' . $originalType;
-            $srcNew = $baseUrl . $this->yellow->system->getHTML('CoreImageLocation') . $newFilename . '.' . $originalType;
+        // Generate new src to image. Inside to convert image
+        // If filter is available and »webp« should use
+        if ($useWebp == true && $isFilterAvailable) {
+            $srcNewInside = $this->yellow->lookup->findMediaDirectory('coreImageLocation') . $choosedFilter . $newFilename . '.webp';
+            $srcNew = $baseUrl . $this->yellow->system->getHTML('CoreImageLocation') . $choosedFilter . $newFilename . '.webp';
         }
 
-        if ($isFilterAvailableExternal || $isFilterAvailableInternal || empty($choosedFilter)) {
+        // If filter is available, use without »webp« or type is not allowed (svg)
+        if (($useWebp != true && ($isFilterAvailable || empty($choosedFilter))) || $isTypeAllowed == false || $compareDefaultCatchImageOriginal === true ){
+            $srcNewInside = $this->yellow->lookup->findMediaDirectory('coreImageLocation') . $choosedFilter . $newFilename . '.' . $originalType;
+            $srcNew = $baseUrl . $this->yellow->system->getHTML('CoreImageLocation') . $choosedFilter . $newFilename . '.' . $originalType;
+        }
+
+        // gernerate/convert new directory and image if nessesary 
+        if (($isFilterAvailable || empty($choosedFilter)) || $choosedFilter == 'webp') {
             $pathNewInside = $this->yellow->lookup->findMediaDirectory('coreImageLocation') . $choosedFilter . '/';
             if (!is_dir($pathNewInside)) {
                 mkdir($pathNewInside);
             }
-
-            if ($isFilterAvailableInternal && $supportedType == '1') {
-                $this->yellow->extension->get('imagefilter')->generateNewImageInternal($choosedFilter, $srcOriginal, $srcNewInside, $originalType);
+            if ($isFilterCatchAvailableInternal || $isFilterDefaultAvailableInternal || $choosedFilter == 'webp') {
+                $this->yellow->extension->get('imagefilter')->generateNewImageInternal($choosedFilter, $srcOriginal, $srcNewInside, $originalType, $isTypeAllowed);
             }
 
-            if ($isFilterAvailableExternal && $supportedType == '1') {
-                $this->yellow->extension->get('imagefilter')->generateNewImageExternal($choosedFilter, $srcOriginal, $srcNewInside, $originalType);
+            if ($isFilterCatchAvailableExternal || $isFilterDefaultAvailableExternal) {
+                $this->yellow->extension->get('imagefilter')->generateNewImageExternal($choosedFilter, $srcOriginal, $srcNewInside, $originalType, $isTypeAllowed);
             }
         }
 
-        $output = '<img class="catchimage" src="';
-        $output .= "$srcNew";
-        $output .= '" alt="';
+        // Generate Output
+        $output = '<img src="';
+        $output .= htmlspecialchars($srcNew);
+        if ($originalType === 'svg') {
+            $output .= '"' . ' ' . 'alt="';
+        } else{
+            $output .= '"' . ' ' . $widthAndHeight . ' ' . 'alt="';
+        }
         if (empty($catchImageAltText)) {
-            $output .= $this->yellow->system->get('catchImageDefaultAltText') . ' ' . $catchImageTitle;
+            $output .= $this->yellow->system->get('catchImageDefaultAltText') . ' ' . htmlspecialchars($catchImageTitle);
         } else {
-            $output .= "$catchImageAltText";
+            $output .= htmlspecialchars($catchImageAltText);
         }
         $output .= "\">\n";
 
         return $output;
     }
 
-    public function checkIfFilterIsAvailableInternal($choosedFilter)
+    // Compare »defaultFilter« with »$catchImageFilter«
+    public function checkIfTwoOriginal($defaultFilter, $catchImageFilter)
     {
-        $isFilterAvailableInternal = method_exists($this->yellow->extension->get('imagefilter'), $choosedFilter);
-        return $isFilterAvailableInternal;
+        if ($defaultFilter === 'original' && $catchImageFilter === 'original'){
+            $compareDefaultCatchImageOriginal = true;
+        }else{
+            $compareDefaultCatchImageOriginal = false;
+        }
+        return $compareDefaultCatchImageOriginal;
     }
 
-    public function checkIfFilterIsAvailableExternal($choosedFilter)
+    // Check if filter is available 
+    public function checkIfFilterIsAvailable($isFilterCatchAvailableExternal, $isFilterCatchAvailableInternal, $isFilterDefaultAvailable)
+    {
+        if ($isFilterCatchAvailableExternal || $isFilterCatchAvailableInternal || $isFilterDefaultAvailable){
+            $isFilterAvailable = true;
+        } else {
+            $isFilterAvailable = false;
+        }
+        return $isFilterAvailable;
+    }
+
+    // Check if filter is available in »imagefilter.php 
+    public function checkIfFilterDefaultIsAvailable($toCheckFilter)
+    {
+        $isFilterCatchAvailableExternal = $this->checkIfFilterIsAvailableExternal($toCheckFilter); 
+        $isFilterCatchAvailableInternal = $this->checkIfFilterIsAvailableInternal($toCheckFilter);
+
+        if (($isFilterCatchAvailableExternal || $isFilterCatchAvailableInternal)) {
+            $isFilterAvailable = true;
+        } else {
+            $isFilterAvailable = false;
+        }
+        return $isFilterAvailable;
+    }
+
+    // Check if the extension »imageFillter« exist and if filter is available in »imagefilter.php«
+    public function checkIfFilterIsAvailableInternal($toCheckFilter)
+    {
+        if ($this->yellow->extension->isExisting('imagefilter')) {
+            $isFilterCatchAvailableInternal = method_exists($this->yellow->extension->get('imagefilter'), $toCheckFilter);
+        } else {
+            $isFilterCatchAvailableInternal = false;
+        }
+        return $isFilterCatchAvailableInternal;
+    }
+    
+    // Check if the extension »imageFillterCollection« exist and if filter is available in »imagefiltercollection.php«
+    public function checkIfFilterIsAvailableExternal($toCheckFilter)
     {
         if ($this->yellow->extension->isExisting('imagefiltercollection')) {
-            $isFilterAvailableExternal = method_exists($this->yellow->extension->get('imagefiltercollection'), $choosedFilter);
+            $isFilterCatchAvailableExternal = method_exists($this->yellow->extension->get('imagefiltercollection'), $toCheckFilter);
         } else {
-            $isFilterAvailableExternal = false;
+            $isFilterCatchAvailableExternal = false;
         }
-        return $isFilterAvailableExternal;
+        return $isFilterCatchAvailableExternal;
+    }
+
+    // Check if Filter contain »imfi« and return filter without »imfi«
+    public function getFilter($toCheckImageFilter)
+    {
+        
+        if ((strpos($toCheckImageFilter, 'imfi') !== false)) {
+            $toCheckImageFilter = strtolower($toCheckImageFilter);
+            $toCheckImageFilter = explode('-', $toCheckImageFilter);
+            $toCheckImageFilter = preg_replace('/\s+/', '', $toCheckImageFilter[1]);
+        } 
+        return $toCheckImageFilter;
     }
 }
